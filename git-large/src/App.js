2401ca75faf918f4c6f601b2b8a87843c609d5ee
ManishKuPatra254 <manishpatra254@gmail.com>
2024-02-02 12:22:10 +0530

    add

---
+++ git-large/src/App.js
@@ -0,0 +1,12 @@
+import logo from './Flow 9@1x-25fps.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
